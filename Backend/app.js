deea7c45bd5c29d6f9693873f57864f4c21a7d55
ManojK1405 <manojkalasgonda7@gmail.com>
2026-02-10 17:53:25 +0530

    Fix CORS configuration: remove extra quotes from allowed origins

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 config({path:"./config/.env"});
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://infinityvillas.vercel.app",""https://real-estate-booking-one.vercel.app""],
+  origin: ["http://localhost:5173", "https://infinityvillas.vercel.app","https://real-estate-booking-one.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   exposedHeaders: ["Content-Type", "Authorization"],
